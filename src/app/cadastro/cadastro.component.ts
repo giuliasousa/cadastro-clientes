e22a241997f61bf1bdc2fe61c597df174e7d8afb
Giulia Sousa <giuliamariatech@gmail.com>
2026-04-08 22:04:54 -0300

    add Angular Material components and start to implement Cadastro and Consulta features

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [FlexLayoutModule, MatCardModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent {
+
+}
